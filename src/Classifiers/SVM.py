c0375293eb91b602a15f0d7a54ba250b5b373c75
Sophia Chan <ceasings@gmail.com>
2017-07-01 16:27:59 -0700

    Modified SVM class to return predictions from an ensemble of SVMs trained on the following features: 	- word (bigrams, trigrams) 	- characters (4-grams, 5-grams) 	- phonemes (4-grams, 5-grams)

--- src/Classifiers/SVM.py
+++ src/Classifiers/SVM.py
@@ -12,21 +12,49 @@ def normalize(x, maximum, minimum):
 class SVM():
     
     def __init__(self):
+        self.clf_list = [
+            _SVM('original', 'word', 2), 
+            _SVM('original', 'word', 3), 
+            _SVM('original', 'char', 4), 
+            _SVM('original', 'char', 5),
+            _SVM('phonemes', 'word', 4), 
+            _SVM('phonemes', 'word', 5),
+            ]
+            
+    def preprocess(self, data):
+        return data
+        
+    def train(self, training_data):
+        for clf in self.clf_list:
+            clf.train(training_data)
+    
+    def classify(self, testing_data):
+        result = [clf.classify(testing_data) for clf in self.clf_list]
+        return result
+        
+        
+class _SVM():
+    
+    def __init__(self, col_name, analyzer, n):
+        tokenize = lambda x: x.split()
+    
         self.pipeline = Pipeline(
-            [('vectorizer', TfidfVectorizer(binary=True, norm='l2')),
+            [('vectorizer', TfidfVectorizer(tokenizer=tokenize, binary=True, norm='l2', ngram_range=(n, n), analyzer=analyzer)),
             ('classifier', LinearSVC())])
+            
+        self.col_name = col_name
     
     def preprocess(self, data):
         return data
     
     def train(self, training_data):
-        result = select_feature(training_data[0][1],training_data[0][0],"original")
+        result = select_feature(training_data[0][1],training_data[0][0], self.col_name)
         info, data = zip(*result)
         labels = [item[2] for item in info]
         self.pipeline.fit(data, labels)
         
     def classify(self, testing_data):
-        result = select_feature(testing_data[0][1], testing_data[0][0], "original")
+        result = select_feature(testing_data[0][1], testing_data[0][0], self.col_name)
         info, data = zip(*result)
         predictions = self.pipeline.decision_function(data)
         minimum = np.min(predictions)
